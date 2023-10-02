a7d93e2964fffbb316be8aef1fabc8e25e3703c3
silviosalza <silviofilipposalza@gmail.com>
2023-10-02 18:05:46 +0200

    added font,style

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,25 @@
 </script>
 
 <template>
-  <div class="app">
-    
+  <div class="app m-5">
+    <div class="title">
+      <h1>Silvio Filippo Salza</h1>
+      <span>Full Stack Junior Web Developer & Not a Proper Designer</span>
+    </div>
+    <div class="nav py-5">
+      <ul>
+        <li>
+          <router-link to="/">Homepage</router-link>
+        </li>
+        <li>
+          <router-link to="/projects">Posts</router-link>
+        </li>
+        <li>
+          <router-link to="/info">Info</router-link>
+        </li>
+      </ul>
+    </div>
     <router-view></router-view>
-    <ul>
-      <li>
-        <router-link to="/">Homepage</router-link>
-      </li>
-      <li>
-        <router-link to="/projects">Posts</router-link>
-      </li>
-      <li>
-        <router-link to="/info">Info</router-link>
-      </li>
-    </ul>
-    
   </div>
 </template>
 
@@ -26,12 +30,21 @@
 @use "./styles/general.scss" as *;
 
 
+.title{
+  h1{
+    font-size: 2rem;
+    font-weight: 100;
+  }
+  span{
+    font-weight: 500;
+  }
+}
 .app {
+  padding: 3rem 0 0 3rem;
   width: calc(100% - 6rem);
   height: calc(100vh - 6rem);
   overflow: hidden;
-  border: black 2px solid;
-  margin: 3rem 3rem;
-
+  border: rgb(81, 81, 81) 1px solid;
+  background-color: #E6E6E6;
 }
 </style>
